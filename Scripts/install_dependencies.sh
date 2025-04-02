c6c53ffafd8b169f403f853585c94098dda3e5d0
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-02 17:26:08 +0100

    modified appspec.yml

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echo "Updating packages..."
 sudo yum update -y
-echo "Updated"
-
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-sudo brew install nvm
-
-sudo nvm install 23
+sudo yum install -y nodejs npm
 
 sudo npm install
 
