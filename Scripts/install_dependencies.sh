9b0fba9b35f3084272d7368efea717d14b561594
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-02 17:12:18 +0100

    modified appspec.yml

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -4,3 +4,14 @@ echo "Updating packages..."
 sudo yum update -y
 echo "Updated"
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install nvm
+
+nvm install 23
+
+npm install
+
+brew tap mongodb/brew
+
+brew install mongodb-community@8.0
