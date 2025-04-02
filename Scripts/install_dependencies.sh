02b60378bf79f093308019aa5a1c3ed4a142a9d9
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-02 17:41:50 +0100

    modified appspec.yml

--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -11,3 +11,14 @@ cd /home/ec2-user/app
 # rm -rf node_modules package-lock.json 
 
 npm install
+
+sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo <<EOF
+[mongodb-org-8.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-8.0.asc
+EOF
+
+sudo yum install -y mongodb-org
