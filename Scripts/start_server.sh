b811646da449f652055dc4d81f0b570231c1386f
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-03 12:46:14 +0100

    modified appspec.yml

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -10,4 +10,7 @@ sudo systemctl start mongod
 pm2 start npm --name "md-app" -- start
 
 pm2 save
-pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/nodejs18/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
+
+sudo systemctl enable pm2-ec2-user
+sudo systemctl start pm2-ec2-user
