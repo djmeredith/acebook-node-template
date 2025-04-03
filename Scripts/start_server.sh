c2a4de29be96aec9a66af5a4c6dcc49244e0ed85
mantaslv <mantasvolkas@gmail.com>
2025-04-03 14:23:15 +0100

    Modified start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -10,8 +10,7 @@ sudo systemctl start mongod
 pm2 start npm --name "md-app" -- start
 
 pm2 save
-pm2 startup
-# sudo env PATH=$PATH:/usr/bin /usr/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
+sudo env PATH=$PATH:/usr/bin /usr/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
 
-# sudo systemctl enable pm2-ec2-user
-# sudo systemctl start pm2-ec2-user
+sudo systemctl enable pm2-ec2-user
+sudo systemctl start pm2-ec2-user
