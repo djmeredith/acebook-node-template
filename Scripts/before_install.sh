eccd99d695fcebb871e62ce8415ab1db9e49e5d3
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-03 12:27:34 +0100

    modified appspec.yml

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-cd /home/ec2-user/app
+cd /home/ec2-user
 
-pm2 stop app
+echo "delete app directory if it exists"
+if [ -d "app" ]; then
+    rm -rf app
+else
+    echo "app does not exist"
+fi
 
 echo "yum update"
 sudo yum update -y
@@ -15,9 +20,9 @@ curl -fsSL https://rpm.nodesource.com/setup_23.x | sudo bash -
 echo "install node and npm"
 sudo yum install -y nodejs npm
 
-echo "delete node modules if they exist"
-if [ -d "node_modules" ]; then
-    rm -rf node_modules
-else
-    echo "node_modules does not exist"
-fi
+# echo "delete node modules if they exist"
+# if [ -d "node_modules" ]; then
+#     rm -rf node_modules
+# else
+#     echo "node_modules does not exist"
+# fi
--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/app
 echo "starting mongo"
 sudo systemctl start mongod
 
-pm2 start npm --name "app" -- start
+pm2 start npm --name "md-app" -- start
 
 pm2 save
 pm2 startup
