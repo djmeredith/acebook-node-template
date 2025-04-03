e9241567fba9c5d5477bde5550dc0ae1e1cf9cca
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-03 11:25:22 +0100

    modified appspec.yml

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -2,14 +2,16 @@
 
 set -e
 
+echo "yum update"
 sudo yum update -y
 
-sudo yum install -y curl
-
+echo "Get node 23 repo"
 curl -fsSL https://rpm.nodesource.com/setup_23.x | sudo bash -
 
+echo "install node and npm"
 sudo yum install -y nodejs npm
 
+echo "delete node modules if they exist"
 if [ -d "/home/ec2-user/app/node_modules" ]; then
     rm -rf /home/ec2-user/app/node_modules
 else
