25356093a664eb8d4551c7d83eca567fa2cefe60
mantaslv <mantasvolkas@gmail.com>
2025-04-03 15:41:07 +0100

    Modified start_server.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -4,27 +4,25 @@ set -e
 
 cd /home/ec2-user
 
-# echo "delete app directory if it exists"
-# if [ -d "app" ]; then
-#     rm -rf app
-# else
-#     echo "app does not exist"
-# fi
-
-# if command -v pm2 &> /dev/null
-# then
-#     echo "PM2 is installed."
+echo "Checking if PM2 is installed..."
+if command -v pm2 &> /dev/null
+then
+    echo "PM2 is installed. Checking for existing processes..."
     
-#     if pm2 list | grep -q 'md-app'
-#     then
-#         echo "PM2 processes are running. Stopping all PM2 processes..."
-#         sudo pm2 delete md-app
-#     else
-#         echo "No PM2 processes are currently running."
-#     fi
-# else
-#     echo "PM2 is not installed or not found in the PATH."
-# fi
+    if pm2 list | grep -q "md-app"; then
+        echo "md-app process found. Stopping and deleting it..."
+        pm2 stop md-app
+        pm2 delete md-app
+    else
+        echo "No md-app process found."
+    fi
+
+    echo "Deleting all PM2 processes just in case..."
+    pm2 stop all
+    pm2 delete all
+else
+    echo "PM2 is not installed, skipping PM2 cleanup."
+fi
 
 sudo yum clean all
 
