1b5b97a463cad08918a309c99984ffd80bc082ad
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-03 11:22:27 +0100

    modified appspec.yml

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+set -e
 
-if [ -d "/home/ec2-user/app" ]; then
-    rm -rf /home/ec2-user/app
+sudo yum update -y
+
+sudo yum install -y curl
+
+curl -fsSL https://rpm.nodesource.com/setup_23.x | sudo bash -
+
+sudo yum install -y nodejs npm
+
+if [ -d "/home/ec2-user/app/node_modules" ]; then
+    rm -rf /home/ec2-user/app/node_modules
 else
-    echo "Directory does not exist"
+    echo "node_modules does not exist"
 fi
-exit 1
--- Scripts/install_dependencies.sh
+++ Scripts/install_dependencies.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-# set -e
+set -e
 
-# sudo yum update -y
+cd /home/ec2-user/app
 
-# sudo yum install -y curl
+npm install
 
-# curl -fsSL https://rpm.nodesource.com/setup_23.x | sudo bash -
+sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo <<EOF
+[mongodb-org-8.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-8.0.asc
+EOF
 
-# sudo yum install -y nodejs npm
-
-# cd /home/ec2-user/app
-
-# # npm cache clean --force
-# # rm -rf node_modules package-lock.json 
-
-# npm install
-
-# sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo <<EOF
-# [mongodb-org-8.0]
-# name=MongoDB Repository
-# baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
-# gpgcheck=1
-# enabled=1
-# gpgkey=https://www.mongodb.org/static/pgp/server-8.0.asc
-# EOF
-
-# sudo yum install -y mongodb-org
-
-if [ -d "app" ]; then
-    echo "Directory exists"
-else
-    echo "Directory does not exist"
-fi
-
-exit 1
+sudo yum install -y mongodb-org
