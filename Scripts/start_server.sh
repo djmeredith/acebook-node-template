14f03ec7c99a2ee56ba3bb1577d798f2763c8c53
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-03 12:30:49 +0100

    modified appspec.yml

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -11,3 +11,4 @@ pm2 start npm --name "md-app" -- start
 
 pm2 save
 pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/nodejs18/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
