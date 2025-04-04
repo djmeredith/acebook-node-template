69e2e37892ab7e10b04693b6497a8a29eed8fc10
mantaslv <mantasvolkas@gmail.com>
2025-04-04 12:04:41 +0100

    Reverted: Modified app.js with port

--- app.js
+++ app.js
@@ -75,10 +75,4 @@ app.use((err, req, res) => {
   res.render("error");
 });
 
-// start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 module.exports = app;
