2c3338ffd55077f07e3bde9cad67163a4d4f9598
mantaslv <mantasvolkas@gmail.com>
2025-04-04 12:45:52 +0100

    Changed back to npm i instead of ci

--- new/Scripts/install_dependencies.sh
+++ new/Scripts/install_dependencies.sh
@@ -4,13 +4,7 @@ set -e
 cd /home/ec2-user/app
 
 echo "Installing Node app dependencies..."
-if git diff --quiet HEAD~1 package.json package-lock.json; then
-    echo "No changes in package files, running npm ci..."
-    npm ci
-else
-    echo "Package files have changed, running npm install..."
-    npm install
-fi
+npm install
 
 echo "Installing PM2 globally..."
 npm install -g pm2
