ed9b0f03372b7b323592cbff9f4fbe6d76b6e6e3
mantaslv <mantasvolkas@gmail.com>
2025-04-04 12:00:57 +0100

    Modified app.js with port

--- new/Scripts/validate_service.sh
+++ new/Scripts/validate_service.sh
@@ -3,16 +3,7 @@ set -e
 
 PORT=3000 
 URL=$(curl -s http://checkip.amazonaws.com/) 
-
-# echo "Checking port availability..."
-# if ss -tuln | grep -q ":$PORT"; then
-#     echo "Port $PORT is open, server not running."
-# 	exit 1
-# else
-#     echo "Port $PORT is not open."
-# fi
-
-sleep 5
+echo "The URL of this ec2 instance is ${URL}"
 
 echo "Checking website exists"
 if curl --silent --fail "http://${URL}:${PORT}"; then
