0120755a2aad985e99f39b38aedec33788c704a3
mantaslv <mantasvolkas@gmail.com>
2025-04-04 12:46:31 +0100

    Commented out validation

--- new/Scripts/validate_service.sh
+++ new/Scripts/validate_service.sh
@@ -5,10 +5,10 @@ PORT=3000
 URL=$(curl -s http://checkip.amazonaws.com/) 
 echo "The URL of this ec2 instance is ${URL}"
 
-echo "Checking website exists"
-if curl --silent --fail "http://${URL}:${PORT}"; then
-    echo "Website is running on port ${PORT}."
-else
-    echo "Website not running."
-    exit 1
-fi
+# echo "Checking website exists"
+# if curl --silent --fail "http://${URL}:${PORT}"; then
+#     echo "Website is running on port ${PORT}."
+# else
+#     echo "Website not running."
+#     exit 1
+# fi
