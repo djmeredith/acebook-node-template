2c7b16deea889f13f96337e07ff0de62d622be94
mantaslv <mantasvolkas@gmail.com>
2025-04-04 11:49:32 +0100

    Added script ot check if dependencies have changed

--- new/Scripts/install_dependencies.sh
+++ new/Scripts/install_dependencies.sh
@@ -4,7 +4,13 @@ set -e
 cd /home/ec2-user/app
 
 echo "Installing Node app dependencies..."
-npm ci
+if git diff --quiet HEAD~1 package.json package-lock.json; then
+    echo "No changes in package files, running npm ci..."
+    npm ci
+else
+    echo "Package files have changed, running npm install..."
+    npm install
+fi
 
 echo "Installing PM2 globally..."
 npm install -g pm2
