ac85fd7bda7eff34ef686a0efbf2a8aadbb97aaa
Dean Meredith <deanjohnmeredith@gmail.com>
2025-04-04 14:51:33 +0100

    update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -5,7 +5,7 @@ PORT=3000
 echo "The port used is ${PORT}"
 URL=$(curl -s http://checkip.amazonaws.com/) 
 echo "The ip address of this ec2 instance is ${URL}"
-
+sleep 5
 
 echo "Checking website exists"
 if curl --silent --fail "http://${URL}:${PORT}"; then
