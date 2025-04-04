0f408dc5b8b7c5994da409659e8f6f98461672e7
mantaslv <mantasvolkas@gmail.com>
2025-04-04 11:54:25 +0100

    Modified validate_service.sh

--- new/Scripts/validate_service.sh
+++ new/Scripts/validate_service.sh
@@ -2,22 +2,22 @@
 set -e
 
 PORT=3000 
-# URL=http://35.179.121.20
+URL=$(curl -s http://checkip.amazonaws.com/) 
 
-echo "Checking port availability..."
-if ss -tuln | grep -q ":$PORT"; then
-    echo "Port $PORT is open, server not running."
-	exit 1
-else
-    echo "Port $PORT is not open."
-fi
-
-# sleep 5
-
-# echo "Checking website exists"
-# if curl —-silent --fail "${URL}:${PORT}" ; then
-#     echo "website is running."
+# echo "Checking port availability..."
+# if ss -tuln | grep -q ":$PORT"; then
+#     echo "Port $PORT is open, server not running."
+# 	exit 1
 # else
-#     echo "Website not runnning."
-#     exit 1
+#     echo "Port $PORT is not open."
 # fi
+
+sleep 5
+
+echo "Checking website exists"
+if curl --silent --fail "http://${URL}:${PORT}"; then
+    echo "Website is running."
+else
+    echo "Website not running."
+    exit 1
+fi
