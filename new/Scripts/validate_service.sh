bfd6ce3e7f3d735f7312d0742f3069166e19c389
mantaslv <mantasvolkas@gmail.com>
2025-04-04 14:32:28 +0100

    Testing validation

--- new/Scripts/validate_service.sh
+++ new/Scripts/validate_service.sh
@@ -3,14 +3,14 @@ set -e
 
 PORT=3000 
 echo "The port used is ${PORT}"
-URL=$(curl -s http://checkip.amazonaws.com/) 
-echo "The ip address of this ec2 instance is ${URL}"
+# URL=$(curl -s http://checkip.amazonaws.com/) 
+# echo "The ip address of this ec2 instance is ${URL}"
 exit 1
 
-# echo "Checking website exists"
-# if curl --silent --fail "http://${URL}:${PORT}"; then
-#     echo "Website is running on port ${PORT}."
-# else
-#     echo "Website not running."
-#     exit 1
-# fi
+echo "Checking website exists"
+if curl --silent --fail "http://35.178.189.165:3000"; then
+    echo "Website is running on port 3000."
+else
+    echo "Website not running."
+    exit 1
+fi
